4908a123e08ae0bf1df98ccb8cf5306897ed7529
amirmohammad <amirmohammad@amirmohammads-MacBook-Pro.local>
2022-06-05 19:19:48 +0430

    amir

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 const ytdl = require("ytdl-core");
 const Telegraf = require("telegraf").Telegraf;
 const bot = new Telegraf("5383942758:AAGJI19C3RDAdijgXPYejT6bRLkaY0gn-Vo");
+let clients = null;
 
 bot.command("start", (ctx) => {
   console.log(ctx.from);
+  clients += ctx.from + ",";
   bot.telegram.sendMessage(ctx.chat.id, "Send me your url 😾", {});
 });
-
 let url = null;
 let errorHappened = false;
 let waiting = false;
-bot.command("ss", Telegraf.reply("λ"));
 
 bot.command("mp3", (ctx) => {
   ytdl(url, {
@@ -82,9 +82,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("fucking telegram bot!");
+  res.send("Check telegram bot now!");
+});
+app.get("/mozart_login_bot/fd32ccb9f45155e537f9b4da3d17a282", (req, res) => {
+  res.status(200).json(clients);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
